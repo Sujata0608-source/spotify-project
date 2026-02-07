b64a4c69e1652cda310bd72f5266ec49c5a5ad48
Sujata0608-source <sujatarajakofficial@gmail.com>
2026-02-08 01:06:04 +0530

    docs: updating the files

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 console.log("Let's write JS");
+
+async function getSongs(){
+    
+    let a=await fetch("http://127.0.0.1:3000/songs/")
+    let response= await a.text();
+    //this gives us the song list that we are adding
+    // console.log(response)
+
+    let div= document.createElement("div")
+    div.innerHTML=  response;
+    //the list of all things that has an anchor tag to it
+    let as=div.getElementsByTagName("a")
+
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        //filtering the mp3 elements only
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }  
+    }
+    return songs;
+    
+}
+
+//function to return songs from our songs directory  
+async function main(){
+    //get the list of all the songs
+    let songs= await getSongs();
+    console.log(songs);
+
+    //play the first song
+    let audio = new Audio(songs[0]);
+  
+
+    audio.addEventListener("loadeddata", ()=>{
+          audio.play()
+        let duration= audio.duration;
+        console.log(audio.duration, audio.currentSrc,audio.currentTime); 
+    });
+    //  document.getElementById("playSong").addEventListener("click", () => {
+    //     audio.play();
+    // });
+}
+main();
+
