59423ce7d73bb6eade5c3f3a05ea17173595bdc9
Sujata0608-source <sujatarajakofficial@gmail.com>
2026-02-23 00:42:31 +0530

    feat: synchronize seekbar circle movement with song playback time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Let's write JS");
-let currentSong=new Audio();
+let currentSong = new Audio();
 
 //function to convert seconds to minutes:seconds format
 function secondsToMinutesSeconds(seconds) {
@@ -17,59 +17,60 @@ function secondsToMinutesSeconds(seconds) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
-async function getSongs(){
-    
-    let a=await fetch("http://127.0.0.1:3000/songs/")
-    let response= await a.text();
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
     //this gives us the song list that we are adding
     // console.log(response)
 
-    let div= document.createElement("div")
-    div.innerHTML=  response;
+    let div = document.createElement("div")
+    div.innerHTML = response;
     //the list of all things that has an anchor tag to it
-    let as=div.getElementsByTagName("a")
-    let songs=[];
-    
+    let as = div.getElementsByTagName("a")
+    let songs = [];
+
     // 
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("songs%5C")[1]);
-        } 
-        
+        }
+
     }
     return songs;
-      
+
 }
 
 //function playMusic
-const playMusic= (track, pause=false)=>{
-// let audio= new Audio("/songs/"+track);   =>this lets all the songs play at once simultaneously if clicked upon, not one at a time
+const playMusic = (track, pause = false) => {
+    // let audio= new Audio("/songs/"+track);   =>this lets all the songs play at once simultaneously if clicked upon, not one at a time
+    currentSong.src = "/songs/" + track;
+    if (!pause) {
+        currentSong.play() // This is skipped because pause is true... GOOD.
+        play.src = "pause.svg"
+    }
+    // currentSong.src="/songs/"+track;
+    // currentSong.play();
 
-if(!pause){
-    currentSong.play() // This is skipped because pause is true... GOOD.
-}
-currentSong.src="/songs/"+track;
-currentSong.play();
-play.src="pause.svg"
-document.querySelector(".songinfo").innerHTML=decodeURI(track)
-document.querySelector(".songtime").innerHTML="00:00 / 00:00"
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
 
 //function to return songs from our songs directory  
-async function main(){
+async function main() {
 
-    
+
     //get the list of all the songs in the form of an array
-    let songs= await getSongs();
+    let songs = await getSongs();
     //to keep a default song playing whenever we reload the page
-   playMusic(songs[0],true)
-   
+    playMusic(songs[0], true)
+
 
     //putting the songs into the songList using JS in the library section
-    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML= songUL.innerHTML + `<li>
+        songUL.innerHTML = songUL.innerHTML + `<li>
         <img class="invert" src="music.svg" alt="">
                             <div class="info">
                                 <div>${decodeURI(song)}</div>
@@ -84,36 +85,42 @@ async function main(){
     //attach and eventlistener to each song
     //we are getting an array of all the elements having 'li' tag to it
     //basically, accessing the lists of songs
-   Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-    e.addEventListener("click",element=>{
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
 
-        console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
 
-        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-        
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
     })
-   })
 
-   //attach an event listener to songbuttons
-   play.addEventListener("click", ()=>{
-    if(currentSong.paused){
-        currentSong.play()
-        play.src="pause.svg"
-    }
-    else{
-        currentSong.pause()
-        play.src="play.svg"
-    }
-   })
+    //attach an event listener to songbuttons
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "play.svg"
+        }
+    })
+
+
+
+
+    //listen for timeupdate event
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    })
 
  
 
 
-    //listen for timeupdate event
-    currentSong.addEventListener("timeupdate", ()=>{
-        console.log(currentSong.currentTime, currentSong.duration);
-        document.querySelector(".songtime").innerHTML=`${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
-    })
 }
 main();
 
