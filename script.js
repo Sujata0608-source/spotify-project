020766035e8688de52ef8ca636fd5c9c7208c746
Sujata0608-source <sujatarajakofficial@gmail.com>
2026-02-22 22:19:34 +0530

    fix: clean song name parsing and resolve autoplay console error

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 console.log("Let's write JS");
 let currentSong=new Audio();
+
+//function to convert seconds to minutes:seconds format
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    // This adds a leading zero if the number is less than 10
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs(){
     
     let a=await fetch("http://127.0.0.1:3000/songs/")
@@ -17,7 +34,7 @@ async function getSongs(){
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("song")[1]);
+            songs.push(element.href.split("songs%5C")[1]);
         } 
         
     }
@@ -26,12 +43,17 @@ async function getSongs(){
 }
 
 //function playMusic
-const playMusic= (track)=>{
+const playMusic= (track, pause=false)=>{
 // let audio= new Audio("/songs/"+track);   =>this lets all the songs play at once simultaneously if clicked upon, not one at a time
 
+if(!pause){
+    currentSong.play() // This is skipped because pause is true... GOOD.
+}
 currentSong.src="/songs/"+track;
 currentSong.play();
 play.src="pause.svg"
+document.querySelector(".songinfo").innerHTML=decodeURI(track)
+document.querySelector(".songtime").innerHTML="00:00 / 00:00"
 }
 
 //function to return songs from our songs directory  
@@ -40,6 +62,8 @@ async function main(){
     
     //get the list of all the songs in the form of an array
     let songs= await getSongs();
+    //to keep a default song playing whenever we reload the page
+   playMusic(songs[0],true)
    
 
     //putting the songs into the songList using JS in the library section
@@ -48,7 +72,7 @@ async function main(){
         songUL.innerHTML= songUL.innerHTML + `<li>
         <img class="invert" src="music.svg" alt="">
                             <div class="info">
-                                <div>${song.replaceAll("%20"," ").replaceAll("s%5C"," ").replaceAll("%C3"," ")}</div>
+                                <div>${decodeURI(song)}</div>
                                 <div>Harry</div>              
                             </div>
                             <div class="playnow">
@@ -82,19 +106,14 @@ async function main(){
     }
    })
 
-    // //play the first song
-    // let audio = new Audio(songs[0]);
-  
+ 
 
-    // audio.addEventListener("loadeddata", ()=>{
-    //     //   audio.play()
-    //     let duration= audio.duration;
-    //     console.log(audio.duration, audio.currentSrc,audio.currentTime); 
-    // });
-    
-    //  document.getElementById("playSong").addEventListener("click", () => {
-    //     audio.play();
-    // });
+
+    //listen for timeupdate event
+    currentSong.addEventListener("timeupdate", ()=>{
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML=`${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+    })
 }
 main();
 
